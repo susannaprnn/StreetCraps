044ccb6d61f33b89f547f0b75400082c84881871
susan <susan@Tobbs.lan>
2016-11-28 20:49:47 +0100

    Red

---
+++ src/Player.java
@@ -0,0 +1,17 @@
+
+public class Player {
+
+		public Player(String name, int cash) {
+		}
+		
+		public String getName(){
+			return "name";
+		}
+		
+		public int getCash(){
+			return 0;
+		}
+
+	}
+
+
---
+++ src/PlayerTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class PlayerTest {
+
+	@Test
+	public void testGetPlayerName() {
+		Player player =  new Player("Petter",300);
+		TestCase.assertTrue(player.getName().equals("Petter"));
+	}
+	
+	@Test
+	public void testGetPlayerCash() {
+		Player player =  new Player("Petter",300);
+		TestCase.assertTrue(player.getCash()==300);
+	}
+}
