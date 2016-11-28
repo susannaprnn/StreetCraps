e481b2867403b1342990abc914460680ba0c3e9c
susan <susan@Tobbs.lan>
2016-11-28 21:09:19 +0100

    Red

--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -1,11 +1,16 @@
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import junit.framework.TestCase;
 
 public class PlayerTest {
-
+	
+	@Rule
+    public ExpectedException thrown= ExpectedException.none();
+	
 	@Test
 	public void testGetPlayerName() {
 		Player player =  new Player("Petter",300);
@@ -17,4 +22,11 @@ public class PlayerTest {
 		Player player =  new Player("Petter",300);
 		TestCase.assertTrue(player.getCash() == 300);
 	}
+	
+	@Test
+	public void testGetPlayerCanNotHaveNegCash() {
+		thrown.expect(NullPointerException.class);
+		Player player =  new Player("Petter",-300);
+	}
 }
+   
