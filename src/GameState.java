12d707b77e611849122b2b0c2dd04c85d89dec92
susan <susan@Tobbs.lan>
2016-11-29 21:19:56 +0100

    Green

--- src/GameState.java
+++ src/GameState.java
@@ -3,17 +3,21 @@ public class GameState {
 	private int round;
 	private int point;
 
-	public GameState(int round, int point){}
+	public GameState(int round, int point){
+		this.round = round;
+		this.point = point;
+	}
 	
 	public void setRound(int round){
-		
+		this.round = round;
 	}
 	public int getRound(){
 		return round;
 	}
 	
-
-	public void setPoint(int point){}
+	public void setPoint(int point){
+		this.point = point;
+	}
 	public int getPoint(){
 		return point;
 	}
